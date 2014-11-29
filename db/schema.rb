4f085a25f70f655c7d8d11e4245d81d7e52c78c0
Michael Probber <michael@probber.com>
2014-11-29 17:52:49 +0300

    vote model generated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141129145059) do
+ActiveRecord::Schema.define(version: 20141129145233) do
 
   create_table "accounts", force: true do |t|
     t.integer  "service_id"
@@ -37,6 +37,14 @@ ActiveRecord::Schema.define(version: 20141129145059) do
   end
 
   create_table "users", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "votes", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "comment_id"
+    t.integer  "points"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
