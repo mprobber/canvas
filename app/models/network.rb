596ad0b270a0f1dac35ef7d228b9f48606e619e1
Michael Probber <michael@probber.com>
2014-11-30 10:51:57 +0300

    removed additional models

--- app/models/network.rb
+++
@@ -1,4 +0,0 @@
-class Network < ActiveRecord::Base
-  has_many :site_networks
-  has_many :sites, through: :site_networks
-end
--- app/models/site_network.rb
+++
@@ -1,4 +0,0 @@
-class SiteNetwork < ActiveRecord::Base
-  belongs_to :site
-  belongs_to :network
-end
