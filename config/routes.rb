059ae2e55e5ea42afd28113e12d13b650eeb6a90
Michael Probber <michael@probber.com>
2014-11-30 13:05:49 +0300

    creating token works

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Canvas::Application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
+  get 'user/token' => 'user#create_token'
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141130081748) do
+ActiveRecord::Schema.define(version: 20141130085913) do
 
   create_table "accounts", force: true do |t|
     t.integer  "service_id"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20141130081748) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_token_id"
+    t.integer  "site_id"
   end
 
   create_table "flags", force: true do |t|
