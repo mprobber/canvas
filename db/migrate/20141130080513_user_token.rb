4d9e6597c7abac76477991f2a5d0fb7c9446402c
Michael Probber <michael@probber.com>
2014-11-30 11:05:54 +0300

    added user token table

---
+++ db/migrate/20141130080513_user_token.rb
@@ -0,0 +1,4 @@
+class UserToken < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141130061634) do
+ActiveRecord::Schema.define(version: 20141130080513) do
 
   create_table "accounts", force: true do |t|
     t.integer  "service_id"
